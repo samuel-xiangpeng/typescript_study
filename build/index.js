df931d174fb51bf18c502c411ea915275dacab4b
samuel.zhou <samuel_xiangpeng@163.com>
2022-10-10 15:05:19 +0800

    ♿️ 初始化目录.安装tsc编译

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function main(x, y) {
+    console.log(x, y);
+    return true;
+}
+main(1, 2);
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+function main(x: number, y: number): Boolean {
+    console.log(x, y);
+    /* 类型推理 */
+    let name = "SAMUEL";
+    const age:number = 20;
+    console.log(name, age);
+    return true
+}
+
+main(1, 2);
